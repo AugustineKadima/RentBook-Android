3824ccceb1197831f6e7a1fd7f5d6d5301d1c1c7
AugustineKadima <kadimasam@gmail.com>
2021-09-05 00:28:56 +0300

    Add onclick listener to main activity get started button

--- app/src/main/java/com/moringaschool/rentbook/MainActivity.java
+++ app/src/main/java/com/moringaschool/rentbook/MainActivity.java
@@ -2,13 +2,29 @@ package com.moringaschool.rentbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+    @BindView(R.id.get_started) Button get_started;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        get_started.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, LandlordSignup.class);
+                startActivity(intent);
+            }
+        });
     }
 }
