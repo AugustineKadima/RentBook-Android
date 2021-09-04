906533767193cec5e83d2fbf0750c3782bb0ba70
AugustineKadima <kadimasam@gmail.com>
2021-09-04 03:28:04 +0300

    Add signup header and name input

---
+++ app/src/main/java/com/moringaschool/rentbook/LandlordSignup.java
@@ -0,0 +1,14 @@
+package com.moringaschool.rentbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LandlordSignup extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landlord_signup);
+    }
+}
