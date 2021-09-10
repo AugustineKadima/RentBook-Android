b8204ea79b123cbad9f4ff3b19c705e6b9c97a9d
AugustineKadima <kadimasam@gmail.com>
2021-09-10 19:28:03 +0300

    update the adapter

--- app/src/main/java/com/moringaschool/rentbook/PropertyAndTenants.java
+++ app/src/main/java/com/moringaschool/rentbook/PropertyAndTenants.java
@@ -33,10 +33,10 @@ public class PropertyAndTenants extends AppCompatActivity {
         add_property.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-//                LinearLayoutManager layoutManager= new LinearLayoutManager(PropertyAndTenants.this, LinearLayoutManager.VERTICAL, false);
-//                TenantsAdapter adapter = new TenantsAdapter(PropertyAndTenants.this, Utils.getAllTenants());
-//                rv_tenants.setLayoutManager(layoutManager);
-//                rv_tenants.setAdapter(adapter);
+                LinearLayoutManager layoutManager= new LinearLayoutManager(PropertyAndTenants.this, LinearLayoutManager.VERTICAL, false);
+                TenantsAdapter adapter = new TenantsAdapter(PropertyAndTenants.this, Utils.getAllTenants());
+                rv_tenants.setLayoutManager(layoutManager);
+                rv_tenants.setAdapter(adapter);
                 Intent intent = new Intent(PropertyAndTenants.this, AddProperty.class);
                 startActivity(intent);
             }
