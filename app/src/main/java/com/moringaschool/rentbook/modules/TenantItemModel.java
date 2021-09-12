312a45bc2d12804278d75eee8a08f2e82918d0b8
AugustineKadima <kadimasam@gmail.com>
2021-09-12 22:06:40 +0300

    Add tenant item model class

---
+++ app/src/main/java/com/moringaschool/rentbook/modules/TenantItemModel.java
@@ -0,0 +1,7 @@
+package com.moringaschool.rentbook.modules;
+
+import android.widget.TextView;
+
+public class TenantItemModel {
+    TextView tenant_display_room, tenant_display_name, tenancy_date_display;
+}
