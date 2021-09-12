8808e61303df4bc82a1bc8a0e7fc4206c697c2e2
AugustineKadima <kadimasam@gmail.com>
2021-09-13 02:51:40 +0300

    Initialize recycler view

--- app/src/main/java/com/moringaschool/rentbook/PropertyList.java
+++ app/src/main/java/com/moringaschool/rentbook/PropertyList.java
@@ -1,14 +1,60 @@
 package com.moringaschool.rentbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.moringaschool.rentbook.adapters.PropertyAdapter;
+import com.moringaschool.rentbook.item_models.PropertyItemModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class PropertyList extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    LinearLayoutManager linearLayoutManager;
+    List<PropertyItemModel> propertyItems;
+    PropertyAdapter propertyAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_property_list);
+
+        initData();
+        initRecyclerView();
+    }
+
+    private void initData() {
+        propertyItems = new ArrayList<>();
+        propertyItems.add(new PropertyItemModel("KimTech Flats", "Kajiado"));
+        propertyItems.add(new PropertyItemModel("Zion Flats", "Kisumu"));
+        propertyItems.add(new PropertyItemModel("Roma Flats", "Nairobi"));
+        propertyItems.add(new PropertyItemModel("Kenya Flats", "Kajiado"));
+        propertyItems.add(new PropertyItemModel("High Flats", "Kidiwaa"));
+        propertyItems.add(new PropertyItemModel("KimTech Flats", "Kajiado"));
+        propertyItems.add(new PropertyItemModel("Zion Flats", "Kisumu"));
+        propertyItems.add(new PropertyItemModel("Roma Flats", "Nairobi"));
+        propertyItems.add(new PropertyItemModel("Kenya Flats", "Kajiado"));
+        propertyItems.add(new PropertyItemModel("High Flats", "Kidiwaa"));
+        propertyItems.add(new PropertyItemModel("KimTech Flats", "Kajiado"));
+        propertyItems.add(new PropertyItemModel("Zion Flats", "Kisumu"));
+        propertyItems.add(new PropertyItemModel("Roma Flats", "Nairobi"));
+        propertyItems.add(new PropertyItemModel("Kenya Flats", "Kajiado"));
+        propertyItems.add(new PropertyItemModel("High Flats", "Kidiwaa"));
+
+    }
+
+    private void initRecyclerView() {
+        recyclerView = findViewById(R.id.rv_property);
+        linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        propertyAdapter = new PropertyAdapter(propertyItems);
+        recyclerView.setAdapter(propertyAdapter);
+        propertyAdapter.notifyDataSetChanged();
     }
 }
--- app/src/main/java/com/moringaschool/rentbook/TenantsList.java
+++ app/src/main/java/com/moringaschool/rentbook/TenantsList.java
@@ -1,14 +1,57 @@
 package com.moringaschool.rentbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.moringaschool.rentbook.adapters.TenantAdapter;
+import com.moringaschool.rentbook.item_models.TenantItemModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TenantsList extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    LinearLayoutManager linearLayoutManager;
+    List<TenantItemModel> tenantItems;
+    TenantAdapter tenantAdapter;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tenants_list);
+
+        initData();
+        initRecyclerView();
+    }
+
+    private void initData() {
+        tenantItems = new ArrayList<>();
+        tenantItems.add(new TenantItemModel("C3", "Mokia", "1/4/1980"));
+        tenantItems.add(new TenantItemModel("C1", "John", "1/4/1980"));
+        tenantItems.add(new TenantItemModel("A3", "Doe", "1/4/1980"));
+        tenantItems.add(new TenantItemModel("V3", "Kevin", "1/4/1980"));
+        tenantItems.add(new TenantItemModel("C4", "Phanice", "1/4/1980"));
+        tenantItems.add(new TenantItemModel("H3", "Dan", "1/4/1980"));
+        tenantItems.add(new TenantItemModel("C6", "Tonny", "1/4/1980"));
+        tenantItems.add(new TenantItemModel("C5", "Johny", "1/4/1980"));
+        tenantItems.add(new TenantItemModel("C9", "Kelvin", "1/4/1980"));
+        tenantItems.add(new TenantItemModel("C12", "Anita", "1/4/1980"));
+        tenantItems.add(new TenantItemModel("C33", "Triza", "1/4/1980"));
+
+    }
+
+    private void initRecyclerView() {
+        recyclerView = findViewById(R.id.rv_tenants);
+        linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        tenantAdapter = new TenantAdapter(tenantItems);
+        recyclerView.setAdapter(tenantAdapter);
+        tenantAdapter.notifyDataSetChanged();
     }
 }
