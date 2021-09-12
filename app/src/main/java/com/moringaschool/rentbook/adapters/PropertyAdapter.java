921a8d80cc568e50847b31eca1b889aa1585262f
AugustineKadima <kadimasam@gmail.com>
2021-09-13 00:46:49 +0300

    Implement property adapter methods

--- app/src/main/java/com/moringaschool/rentbook/adapters/PropertyAdapter.java
+++ app/src/main/java/com/moringaschool/rentbook/adapters/PropertyAdapter.java
@@ -1,4 +1,38 @@
 package com.moringaschool.rentbook.adapters;
 
-public class PropertyAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.moringaschool.rentbook.item_models.PropertyItemModel;
+
+import java.util.List;
+
+public class PropertyAdapter extends RecyclerView.Adapter<PropertyAdapter.ViewHolder> {
+
+    List<PropertyItemModel> propertyList;
+
+    @NonNull
+    @Override
+    public PropertyAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull PropertyAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
