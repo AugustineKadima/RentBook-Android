cce0858b44b77a96aa4db5be73fe3c68fffe5188
AugustineKadima <kadimasam@gmail.com>
2021-09-13 00:49:37 +0300

    Implement tenant adapter methods

--- app/src/main/java/com/moringaschool/rentbook/adapters/TenantAdapter.java
+++ app/src/main/java/com/moringaschool/rentbook/adapters/TenantAdapter.java
@@ -1,4 +1,31 @@
 package com.moringaschool.rentbook.adapters;
 
-public class TenantAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class TenantAdapter extends RecyclerView.Adapter<TenantAdapter.ViewHolder>{
+    @NonNull
+    @Override
+    public TenantAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TenantAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
