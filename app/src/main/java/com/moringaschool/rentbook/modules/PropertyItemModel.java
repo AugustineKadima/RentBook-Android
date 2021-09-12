ca60c695a971fbe66014b0180e1519a324f2c58d
AugustineKadima <kadimasam@gmail.com>
2021-09-12 22:55:28 +0300

    Add a property item  model class

---
+++ app/src/main/java/com/moringaschool/rentbook/modules/PropertyItemModel.java
@@ -0,0 +1,29 @@
+package com.moringaschool.rentbook.modules;
+
+import android.widget.TextView;
+
+public class PropertyItemModel {
+
+    TextView property_display_name,property_display_location;
+
+    public PropertyItemModel(TextView property_display_name, TextView property_display_location) {
+        this.property_display_name = property_display_name;
+        this.property_display_location = property_display_location;
+    }
+
+    public TextView getProperty_display_name() {
+        return property_display_name;
+    }
+
+    public void setProperty_display_name(TextView property_display_name) {
+        this.property_display_name = property_display_name;
+    }
+
+    public TextView getProperty_display_location() {
+        return property_display_location;
+    }
+
+    public void setProperty_display_location(TextView property_display_location) {
+        this.property_display_location = property_display_location;
+    }
+}
