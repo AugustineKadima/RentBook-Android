9f381a3b002d6f8959377eb6749a8b46a97a194a
AugustineKadima <kadimasam@gmail.com>
2021-09-18 22:17:14 +0300

    Add login ogin authentification

--- app/src/main/java/com/moringaschool/rentbook/Login.java
+++ app/src/main/java/com/moringaschool/rentbook/Login.java
@@ -1,14 +1,19 @@
 package com.moringaschool.rentbook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.BindView;
@@ -42,13 +47,31 @@ public class Login extends AppCompatActivity {
                 }else if(Email.isEmpty()){
                     login_email.setError("Email required");
                     login_email.requestFocus();
-                }else if(Password.isEmpty()){
+                }else if(!Patterns.EMAIL_ADDRESS.matcher(Email).matches()){
+                    login_email.setError("Please provide a valid email");
+                    login_email.requestFocus();
+                }
+
+                else if(Password.isEmpty()){
                     login_password.setError("Password required");
                     login_password.requestFocus();
+                }else if(Password.length() < 6){
+                    login_password.setError("Password should contain a minimum of 6 values");
                 }
                 else{
-                    Intent intent = new Intent(Login.this, PropertyAndTenants.class);
-                    startActivity(intent);
+                    mAuth.signInWithEmailAndPassword(Email,Password)
+                        .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if(task.isSuccessful()){
+                                    Intent intent = new Intent(Login.this, PropertyAndTenants.class);
+                                    startActivity(intent);
+                                }
+                                else{
+                                    Toast.makeText(Login.this, "Failed to login! Please check your credentials.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
                 }
             }
         });
